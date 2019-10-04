691ba0f471cce07abd512fde507b70d0e5f54885
Thomas Young <yk_ecust_2007@163.com>
2019-10-04 16:40:56 +0800

    [feature] 模板done
    * 上下文
    * 全局对象
    * 过滤器
    * 测试器
    * 模板环境对象
    * 局部模板, 宏, 模板继承
    * js和css中jinja2的使用

--- demos/template/app.py
+++ demos/template/app.py
@@ -1,13 +1,16 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup, flash, redirect, url_for
+from loguru import logger
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 user = {
-    'username': 'Grey Li',
-    'bio': 'A boy who loves movies and music.',
+    'username': 'Thomas Young',
+    'bio': 'A man who love math and freedom.',
 }
 
 movies = [
@@ -26,9 +29,86 @@ movies = [
 
 @app.route('/watchlist')
 def watchlist():
-    return render_template('watchlist.html', user=user, movies=movies)
+    markup_text = Markup('<h3>Hello, Flask!<h3>')
+    return render_template('watchlist.html', user=user, movies=movies, p_value='<p>content</p>',
+                           markup_text=markup_text)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/watchlist2')
+def watchlist_with_static():
+    return render_template('watchlist_with_static.html', user=user, movies=movies)
+
+
+# message flashing
+@app.route('/flash')
+def just_flash():
+    flash('I am flash, who is looking for me?')
+    flash('我是杨恺')
+    return redirect(url_for('index'))
+
+
+# register template context handler
+@app.context_processor
+def inject_foo():
+    foo = "this is foo"
+    return {"foo": foo, 'alert_color': 'purple'}  # alert_color用于传给css的自定义变量--alert-color, 设置flash的颜色
+
+
+# 向模板注入函数1
+@app.context_processor
+def inject_fibonacci():
+    logger.info('invoke inject_fabonaci')
+
+    def fibonacci(n):
+        a, b = 1, 1
+        i = 1
+        while i <= n:
+            a, b = b, a + b
+            i += 1
+        return a
+
+    return dict(fibonacci=fibonacci)
+
+
+# 向模板注入函数2
+@app.template_global()
+def bar():
+    logger.info('invoke bar')
+    return 'I am a bar'
+
+
+# register template filter
+@app.template_filter()
+def musical(s):
+    return s + Markup(' &#9835;')
+
+
+def laugh(s, n=1):
+    return s + ' ,ha' * n + '!'
+
+
+# register template filter
+app.add_template_filter(laugh)
+
+
+# register template test
+@app.template_test()
+def baz(n):
+    if n == 'baz':
+        return True
+    return False
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
---
+++ tests/test_demos/test_template.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    a, b = 1, 1
+    i = 1
+    while i <= n:
+        a, b = b, a + b
+        i += 1
+    return a
+
+
+def test_fabonaci():
+    assert fibonacci(0) == 1
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 2
+    assert fibonacci(3) == 3
+    assert fibonacci(4) == 5
+    assert fibonacci(5) == 8
+    assert fibonacci(6) == 13
