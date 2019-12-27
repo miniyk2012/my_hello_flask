089bc9f3e3b4a25540959660e267d5de34e08287
Thomas Young <yk_ecust_2007@163.com>
2019-12-27 08:02:12 +0800

    mail

--- demos/email/app.py
+++ demos/email/app.py
@@ -1,9 +1,10 @@
 import os
 from threading import Thread
-
+import sendgrid
 from flask import Flask, request, flash, render_template, url_for
 from flask_mail import Mail, Message
 from flask_wtf import FlaskForm
+from sendgrid.helpers.mail import Mail as SGMail
 from werkzeug.utils import redirect
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
@@ -11,17 +12,38 @@ from wtforms.validators import DataRequired, Email
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
-
 app.config.update(
-    SECRET_KEY=os.getenv('SECRET_KEY', 'secret string'),
-    MAIL_SERVER=os.getenv('MAIL_SERVER'),
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
-    MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),
-    MAIL_DEFAULT_SENDER=('Thomas Young', os.getenv('MAIL_USERNAME'))
-)
-mail = Mail(app)
+    SECRET_KEY=os.getenv('SECRET_KEY', 'secret string'))
+
+
+def config_netease_mail():
+    app.config.update(
+        MAIL_SERVER=os.getenv('MAIL_SERVER'),
+        MAIL_PORT=465,
+        MAIL_USE_SSL=True,
+        MAIL_USE_TLS=False,
+        MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
+        MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),
+        MAIL_DEFAULT_SENDER=('Thomas Young', os.getenv('MAIL_USERNAME'))
+    )
+    global mail
+    mail = Mail(app)
+    return mail
+
+
+def config_sendgrid_mail():
+    app.config.update(
+        MAIL_SERVER='smtp.sendgrid.net',
+        MAIL_PORT=587,
+        MAIL_USE_TLS=True,
+        MAIL_USE_SSL=False,
+        MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
+        MAIL_PASSWORD=os.getenv('SENDGRID_API_KEY'),
+        MAIL_DEFAULT_SENDER=('Thomas Young', os.getenv('MAIL_USERNAME'))
+    )
+    global mail
+    mail = Mail(app)
+    return mail
 
 
 # send over SMTP
@@ -29,6 +51,7 @@ def send_smtp_mail(subject, to, body):
     """from app import send_smtp_mail
     send_smtp_mail('hello', '812350401@qq.com', '你好呀')
     """
+    mail = config_netease_mail()
     message = Message(subject, recipients=[to], body=body)
     mail.send(message)
 
@@ -49,12 +72,32 @@ class EmailForm(FlaskForm):
 
 
 def send_api_mail(subject, to, body):
-    pass
+    # mail = config_sendgrid_mail()
+    # message = Message(subject, recipients=[to], body=body)
+    # mail.send(message)
+    send_by_api(body, subject, to)
+
+
+def send_by_api(body, subject, to):
+    sg = sendgrid.SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+    from_email = 'ThomasYoung <yk_ecust_2007@163.com>'
+    text_content = body
+    html_content = '<h1>订阅</h1>'
+    print(body, subject, to)
+    email = SGMail(from_email=from_email, subject=subject, to_emails=to, plain_text_content=text_content,
+                   html_content=html_content)
+    response = sg.send(email)
+
+    print('code', response.status_code)
+    print('body', response.body)
+    print('headers', response.headers)
 
 
 # send email asynchronously
 def _send_async_mail(app, subject, to, body):
     with app.app_context():
+        mail = config_netease_mail()
+        config_netease_mail()
         message = Message(subject, recipients=[to], body=body)  # 我发现这一步特别慢, 因此也搬到另一个线程里面来
         mail.send(message)
 
@@ -67,6 +110,7 @@ def send_async_mail(subject, to, body):
 
 # send email with HTML body
 def send_subscribe_mail(subject, to, **kwargs):
+    mail = config_netease_mail()
     message = Message(subject, recipients=[to], sender='Flask Weekly <%s>' % os.getenv('MAIL_USERNAME'))
     message.body = render_template('emails/subscribe.txt', **kwargs)
     message.html = render_template('emails/subscribe.html', **kwargs)
